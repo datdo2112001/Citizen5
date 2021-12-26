783ab2e65cdc2a8d4aee7a7b71ea1971a97d0507
Dat Do <datdobn58211@gmail.com>
2021-12-27 00:19:43 +0700

    final version

--- db/migrate/20211213152815_add_index_to_users_accountname.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToUsersAccountname < ActiveRecord::Migration[6.1]
-  def change
-    add_index :users, :accountname, unique: true
-  end
-end
--- db/migrate/20211226142537_add_count_to_locals.rb
+++
@@ -1,5 +0,0 @@
-class AddCountToLocals < ActiveRecord::Migration[6.1]
-  def change
-    add_column :locals, :count, :integer
-  end
-end
